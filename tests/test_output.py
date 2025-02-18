5357cd46dfe5bc1e368ef671e9000cb4e20bb52b
stuffbyyuki <kake.19940627@gmail.com>
2025-02-18 15:20:59 -0700

    test updated

--- tests/test_output.py
+++ tests/test_output.py
@@ -2,7 +2,7 @@ import pytest
 import polars as pl
 import duckdb
 from polars.testing import assert_frame_equal
-from duckdb_vs_polars_vs_fireducks.config import DATA_FILE_PATH_STR, POLARS_DTYPES
+from duckdb_vs_polars_vs_fireducks.config import DATASET_NAME, POLARS_DTYPES
 import pandas as pd
 
 
@@ -12,7 +12,7 @@ def df():
     Creates a base Polars DataFrame that all tests will use.
     """
     # Polars code
-    return pl.scan_csv(DATA_FILE_PATH_STR, schema=POLARS_DTYPES).head(1000).collect()
+    return pl.scan_csv(f"data/{DATASET_NAME}.csv", schema=POLARS_DTYPES).head(1000).collect()
 
 
 def test_agg(df):
@@ -26,7 +26,7 @@ def test_agg(df):
         min=pl.col("total_amount").min(),
         max=pl.col("total_amount").max(),
     )
-    
+
     # DuckDB code
     query = f"""
         select 
@@ -48,7 +48,9 @@ def test_agg(df):
     )
 
     assert_frame_equal(polars_df, duckdb_df, check_row_order=False, check_dtypes=False)
-    assert_frame_equal(polars_df, pl.from_pandas(pandas_df), check_row_order=False, check_dtypes=False)
+    assert_frame_equal(
+        polars_df, pl.from_pandas(pandas_df), check_row_order=False, check_dtypes=False
+    )
 
 
 def test_groupby_agg(df):
@@ -64,7 +66,7 @@ def test_groupby_agg(df):
         max=pl.col("total_amount").max(),
     )
     print(polars_df.head())
-    
+
     # DuckDB code
     query = f"""
         select 
@@ -95,7 +97,9 @@ def test_groupby_agg(df):
     )
 
     assert_frame_equal(polars_df, duckdb_df, check_row_order=False, check_dtypes=False)
-    assert_frame_equal(polars_df, pl.from_pandas(pandas_df), check_row_order=False, check_dtypes=False)
+    assert_frame_equal(
+        polars_df, pl.from_pandas(pandas_df), check_row_order=False, check_dtypes=False
+    )
 
 
 def test_window_func(df):
@@ -109,7 +113,7 @@ def test_window_func(df):
         .rank(method="dense", descending=True)
         .over("payment_type"),
     )
-    
+
     # DuckDB code
     query = f"""
         select 
@@ -121,23 +125,25 @@ def test_window_func(df):
     duckdb_df = (
         duckdb.sql(query)
         .pl()
-        .with_columns(pl.col("ttl_amt_rank_per_pay_type").cast(pl.UInt32))
+        .with_columns(pl.col("ttl_amt_rank_per_pay_type"))
     )
 
     # Pandas code
     pandas_df = pd.DataFrame(
         {
-            "avg_fare_per_vendor": df.to_pandas().groupby("VendorID")["fare_amount"].transform(
-                "mean"
-            ),
-            "ttl_amt_rank_per_pay_type": df.to_pandas().groupby("payment_type")[
-                "total_amount"
-            ].rank(method="dense", ascending=False),
+            "avg_fare_per_vendor": df.to_pandas()
+            .groupby("VendorID")["fare_amount"]
+            .transform("mean"),
+            "ttl_amt_rank_per_pay_type": df.to_pandas()
+            .groupby("payment_type")["total_amount"]
+            .rank(method="dense", ascending=False),
         }
     )
 
     assert_frame_equal(polars_df, duckdb_df, check_row_order=False, check_dtypes=False)
-    assert_frame_equal(polars_df, pl.from_pandas(pandas_df), check_row_order=False, check_dtypes=False)
+    assert_frame_equal(
+        polars_df, pl.from_pandas(pandas_df), check_row_order=False, check_dtypes=False
+    )
 
 
 def test_join(df):
@@ -182,7 +188,6 @@ def test_join(df):
     """
     duckdb_df = duckdb.sql(query).pl()
 
-    
     # Pandas code
     pdf = df.to_pandas()
     pdf["pickup_month"] = pd.to_datetime(
@@ -201,4 +206,6 @@ def test_join(df):
     )
 
     assert_frame_equal(polars_df, duckdb_df, check_row_order=False, check_dtypes=False)
-    assert_frame_equal(polars_df, pl.from_pandas(pandas_df), check_row_order=False, check_dtypes=False)
+    assert_frame_equal(
+        polars_df, pl.from_pandas(pandas_df), check_row_order=False, check_dtypes=False
+    )
