a7317bd21ed4526c92c46d97207ae824f5a2d77e
Yuki Kakegawa <yuki@jumpapp.com>
2026-01-06 21:28:05 -0700

    orged polars model

--- local-first-analytics-stack/sqlmesh/models/full_model_polars.py
+++ local-first-analytics-stack/sqlmesh/models/full_model_polars.py
@@ -23,6 +23,7 @@ from sqlglot.expressions import to_column
         "zip_code": "text",
         "contributing_factor": "text",
         "vehicle_type": "text",
+        "collision_count": "bigint",
         "total_number_of_persons_injured": "bigint",
         "total_number_of_persons_killed": "bigint",
         "_lakehouse_loaded_at": "timestamp",
@@ -30,16 +31,17 @@ from sqlglot.expressions import to_column
     column_descriptions={
         "crash_date": "Occurrence date of collision",
         "zip_code": "Postal code of incident occurrence",
+        "collision_count": "Total number of collisions",
         "total_number_of_persons_injured": "Total number of persons injured",
         "total_number_of_persons_killed": "Total number of persons killed",
         "contributing_factor": "Factors contributing to the collision for designated vehicle",
         "vehicle_type": "Type of vehicle based on the selected vehicle category (ATV, bicycle, car/suv, ebike, escooter, truck/bus, motorcycle, other)",
         "_lakehouse_loaded_at": "Timestamp of when the record was loaded into the lakehouse",
     },
-    # audits=[
-    #     ("unique_combination_of_columns", {"columns": [to_column("crash_date", "zip_code", "contributing_factor", "vehicle_type")]}),
-    #     ("not_null", {"columns": [to_column("crash_date")]})
-    # ],
+    audits=[
+        ("unique_combination_of_columns", {"columns": [to_column("crash_date"), to_column("zip_code"), to_column("contributing_factor"), to_column("vehicle_type")]}),
+        ("not_null", {"columns": [to_column("crash_date")]})
+    ],
 )
 def execute(
     context: ExecutionContext,
@@ -55,7 +57,7 @@ def execute(
     """
     source = context.resolve_table("lakehouse.incremental_model")
 
-    pdf = context.fetchdf(
+    pandas_df = context.fetchdf(
         f"""
         SELECT
           crash_date,
@@ -66,12 +68,11 @@ def execute(
           number_of_persons_killed
         FROM {source}
         """
-    )
+    ) 
 
-    df = pl.from_pandas(pdf)
-
-    out = (
-        df.group_by(
+    df = (
+        pl.from_pandas(pandas_df)
+        .group_by(
             ["crash_date", "zip_code", "contributing_factor_vehicle_1", "vehicle_type_code1"],
             maintain_order=False,
         )
@@ -101,6 +102,6 @@ def execute(
         )
     )
 
-    return out.to_pandas()
+    return df.to_pandas()
 
 
