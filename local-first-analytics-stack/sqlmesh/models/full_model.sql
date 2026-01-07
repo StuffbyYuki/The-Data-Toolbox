a79fdaf643dbcbd2854492b958121aabbdffc61a
Yuki Kakegawa <yuki@jumpapp.com>
2026-01-06 21:12:51 -0700

    changed from single quotes to double quotes

--- local-first-analytics-stack/sqlmesh/models/full_model.sql
+++ local-first-analytics-stack/sqlmesh/models/full_model.sql
@@ -1,7 +1,7 @@
 MODEL (
   name lakehouse.full_model,
   kind FULL, 
-  cron '@daily',  -- this model runs daily at 00:00 UTC
+  cron "@daily",  -- this model runs daily at 00:00 UTC
   grain (crash_date, zip_code, contributing_factor, vehicle_type),
   partitioned_by crash_date,
   clustered_by (contributing_factor, vehicle_type, zip_code),
@@ -10,13 +10,13 @@ MODEL (
     - Aggregates over collisions by date, borough, contributing factor, and vehicle type.
   """,
   column_descriptions (
-    crash_date = 'Occurrence date of collision',
-    zip_code = 'Postal code of incident occurrence',
-    total_number_of_persons_injured = 'Total number of persons injured',
-    total_number_of_persons_killed = 'Total number of persons killed',
-    contributing_factor = 'Factors contributing to the collision for designated vehicle',
-    vehicle_type = 'Type of vehicle based on the selected vehicle category (ATV, bicycle, car/suv, ebike, escooter, truck/bus, motorcycle, other)',
-    _lakehouse_loaded_at = 'Timestamp of when the record was loaded into the lakehouse',
+    crash_date = "Occurrence date of collision",
+    zip_code = "Postal code of incident occurrence",
+    total_number_of_persons_injured = "Total number of persons injured",
+    total_number_of_persons_killed = "Total number of persons killed",
+    contributing_factor = "Factors contributing to the collision for designated vehicle",
+    vehicle_type = "Type of vehicle based on the selected vehicle category (ATV, bicycle, car/suv, ebike, escooter, truck/bus, motorcycle, other)",
+    _lakehouse_loaded_at = "Timestamp of when the record was loaded into the lakehouse",
   ),
   audits (
     unique_combination_of_columns(columns = (crash_date, zip_code, contributing_factor, vehicle_type)),
